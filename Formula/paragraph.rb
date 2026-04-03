e2eb659a279eb57cd75e200b4155b9f0f80e2849
Dario Zubillaga <dariozubi@gmail.com>
2026-04-03 13:11:10 -0600

    paragraph v0.0.3

--- Formula/paragraph.rb
+++ Formula/paragraph.rb
@@ -1,18 +1,18 @@
 class Paragraph < Formula
   desc "CLI for Paragraph — manage posts, subscribers, and publications"
   homepage "https://github.com/paragraph-xyz/paragraph-cli"
-  url "https://github.com/paragraph-xyz/paragraph-cli/releases/download/v0.0.2/paragraph-cli-v0.0.2.tgz"
-  sha256 "7a202d8a82619bbb60e73be3b9e32a8a142c37d1284915e35108b48a657c4a47"
+  url "https://github.com/paragraph-xyz/paragraph-cli/releases/download/v0.0.3/paragraph-cli-v0.0.3.tgz"
+  sha256 "167efbf3b0b74141c35827ca8d4995e65f8b44f05b087123ecefbc65f24e4403"
   license "MIT"
 
   depends_on "node"
 
   def install
     system "npm", "install", *std_npm_args
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    bin.install_symlink Dir["\#{libexec}/bin/*"]
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/paragraph --version").strip
+    assert_match version.to_s, shell_output("\#{bin}/paragraph --version").strip
   end
 end
