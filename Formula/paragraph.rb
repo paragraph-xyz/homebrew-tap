36a353e05c20e60be1e6a47ea20cf0af7cf320f6
Dario Zubillaga <dariozubi@gmail.com>
2026-04-16 17:19:31 -0600

    paragraph v0.1.0

--- Formula/paragraph.rb
+++ Formula/paragraph.rb
@@ -1,8 +1,8 @@
 class Paragraph < Formula
   desc "CLI for Paragraph — manage posts, subscribers, and publications"
   homepage "https://github.com/paragraph-xyz/paragraph-cli"
-  url "https://github.com/paragraph-xyz/paragraph-cli/releases/download/v0.0.4/paragraph-cli-v0.0.4.tgz"
-  sha256 "9a42143b8eb50d2dfa8fa9d0050a107247fe8ee67a85d4535137f11f3546337c"
+  url "https://github.com/paragraph-xyz/paragraph-cli/releases/download/v0.1.0/paragraph-cli-v0.1.0.tgz"
+  sha256 "056064a1a25232970bedefdba6c7087bc54ebce264bdb98ba4c9d753e79be7a6"
   license "MIT"
 
   depends_on "node"
